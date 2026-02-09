19eb7788a2871067fd5a9f7aaef0017f60fbefd8
DarthegX <egarrigosc@gmail.com>
2026-02-09 23:51:28 +0100

    Added router

--- apps/claims-front/src/app/app.routes.ts
+++ apps/claims-front/src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ClaimListComponent } from './pages/claim-list/claim-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'claims-list',
+        component: ClaimListComponent,
+    },
+    {
+        path: '',
+        redirectTo: 'claims-list',
+        pathMatch: 'full',
+    },
+    {
+        path: '**',
+        redirectTo: 'claims-list',
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
