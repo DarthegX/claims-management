a1b9d6575ded62b0f39f2d766f7e55886f2badb4
DarthegX <egarrigosc@gmail.com>
2026-02-13 21:49:58 +0100

    Some typing fixes

--- apps/claims-api/src/claims/application/damage.service.ts
+++ apps/claims-api/src/claims/application/damage.service.ts
@@ -2,15 +2,14 @@ import { Injectable } from "@nestjs/common";
 import { CreateDamageDto } from "../dto/create-damage.dto";
 import { InjectModel } from "@nestjs/mongoose";
 import { Claim } from "../domain/claim.entity";
-import { DamageDocument } from "../persistence/schemas/damage.schema";
+import { Damage, DamageDocument } from "../persistence/schemas/damage.schema";
 import { Model } from 'mongoose';
-import { Damage } from "../domain/damage.entity";
 import { Severity } from "../domain/severity.enum";
 import { UpdateDamageDto } from "../dto/update-damage.dto";
 
 @Injectable()
 export class DamagesService {
-    constructor(@InjectModel(Claim.name) private damageModel: Model<DamageDocument>,) { }
+    constructor(@InjectModel(Damage.name) private damageModel: Model<DamageDocument>,) { }
 
     async createDamage(damageDto: CreateDamageDto): Promise<string> {
         const createdDamage = await this.damageModel.create(damageDto);
@@ -18,15 +17,15 @@ export class DamagesService {
         return createdDamage._id.toString();
     }
 
-    findHighSeverityDamagesByClaim(damages: Damage[]): boolean {
-        const highDamage = damages.find((damage) => { damage.severity === Severity.HIGH })
+    findHighSeverityDamagesByClaim(damages: any[]): boolean {
+        const highDamage = damages.find((damage) => damage.severity === Severity.HIGH)
 
-        return highDamage !== null;
+        return highDamage !== undefined;
     }
 
     async updateDamage(damageId: string, updateDamageDto: UpdateDamageDto): Promise<void> {
         const updatedDamage = await this.damageModel.findByIdAndUpdate(damageId, updateDamageDto);
-        console.log('Update damage: ' + updatedDamage?.toJSON());
+
         return;
     }
 
